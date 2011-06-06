8a7497f7f88a3a7b90e3e7473248d62b8ab77e2a
Corey Oordt <coreyoordt@gmail.com>
2011-06-06 07:37:11 -0400

    Inital Commit

---
+++ settings.py
@@ -0,0 +1,112 @@
+# Django settings for project project.
+
+import calloway
+import os
+import sys
+
+CALLOWAY_ROOT = os.path.abspath(os.path.dirname(calloway.__file__))
+sys.path.insert(0, os.path.join(CALLOWAY_ROOT, 'apps'))
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'apps'))
+sys.path.insert(0, os.path.join(PROJECT_ROOT, 'lib'))
+
+try:
+    from local_settings import DEBUG as LOCAL_DEBUG
+    DEBUG = LOCAL_DEBUG
+except ImportError:
+    DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+from calloway.settings import *
+
+ADMINS = (
+    ('django', 'coreyoordt@gmail.com'),
+)
+MANAGERS = ADMINS
+DEFAULT_FROM_EMAIL='coreyoordt@gmail.com'
+SERVER_EMAIL='coreyoordt@gmail.com'
+
+SECRET_KEY = '_+y!a7o3nydbv_5=z5(mf#2ww$c9^)52ms98q%bmol=5r2qwhx'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', 
+        'NAME': 'dev.db',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+TIME_ZONE = 'America/New_York'
+LANGUAGE_CODE = 'en-us'
+USE_I18N = True
+
+try:
+    from local_settings import MEDIA_URL_PREFIX
+except ImportError:
+    MEDIA_URL_PREFIX = "/media/"
+try:
+    from local_settings import MEDIA_ROOT_PREFIX
+except ImportError:
+    MEDIA_ROOT_PREFIX = os.path.join(PROJECT_ROOT, 'media')
+try:    
+    from local_settings import MEDIA_ROOT
+except ImportError:
+    MEDIA_ROOT = os.path.join(MEDIA_ROOT_PREFIX, 'uploads')
+try:
+    from local_settings import STATIC_ROOT
+except ImportError:
+    STATIC_ROOT = os.path.join(MEDIA_ROOT_PREFIX, 'static')
+    
+
+MEDIA_URL = '%suploads/' % MEDIA_URL_PREFIX
+STATIC_URL = "%sstatic/" % MEDIA_URL_PREFIX
+STATIC_MEDIA_APP_MEDIA_PATH = STATIC_ROOT
+STATIC_MEDIA_COPY_PATHS = (
+    {'from': os.path.join(CALLOWAY_ROOT, 'media'), 'to': STATIC_ROOT},
+    {'from': 'static', 'to': STATIC_ROOT},
+)
+STATIC_MEDIA_COMPRESS_CSS = not DEBUG
+STATIC_MEDIA_COMPRESS_JS = not DEBUG
+
+MMEDIA_DEFAULT_STORAGE = 'media_storage.MediaStorage'
+MMEDIA_IMAGE_UPLOAD_TO = 'image/%Y/%m/%d'
+
+AUTH_PROFILE_MODULE = ''
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates'),
+) + CALLOWAY_TEMPLATE_DIRS
+
+CACHE_BACKEND = 'memcached://localhost:11211/'
+
+INSTALLED_APPS = APPS_CORE + \
+    APPS_ADMIN + \
+    APPS_STAFF + \
+    APPS_REVERSION + \
+    APPS_STORIES + \
+    APPS_CALLOWAY_DEFAULT + \
+    APPS_MPTT + \
+    APPS_CATEGORIES + \
+    APPS_COMMENT_UTILS + \
+    APPS_FRONTEND_ADMIN + \
+    APPS_MEDIA + \
+    APPS_UTILS + \
+    APPS_REGISTRATION + \
+    APPS_TINYMCE 
+
+ADMIN_TOOLS_THEMING_CSS = 'admin/css/theming.css'
+# ADMIN_TOOLS_MENU = 'menu.CustomMenu'
+
+TINYMCE_JS_URL = '%sjs/tiny_mce/tiny_mce.js' % STATIC_URL
+TINYMCE_JS_ROOT = os.path.join(STATIC_ROOT, 'js/tiny_mce')
+
+try:
+    from local_settings import *
+except ImportError:
+    pass
